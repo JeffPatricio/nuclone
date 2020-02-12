3d555a137f6dc3af98026ddb08278762498a715e
Jefferson Patrício <jeffersonpatricio1@gmail.com>
2020-02-12 16:43:52 -0300

    modificação em splash screen

--- src/components/Tabs/styles.js
+++ src/components/Tabs/styles.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/native';
 export const Container = styled(Animated.View)`
   height: 100px;
   margin-top: 20px;
+  margin-bottom: 10px;
 `;
 
 export const TabsContainer = styled.ScrollView.attrs({
